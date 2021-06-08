de43ffc9e7aa68311e08e23ab1c28eab40144ad6
Vladimir Budylnikov <me@nett00n.org>
2021-06-08 21:44:52 +0300

    update to 1.05.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-﻿$packageName = 'vkQuake'
+$packageName = 'vkQuake'
 $toolsPath   = Split-Path $MyInvocation.MyCommand.Definition
 $vkQuake_folder = "$(Get-ToolsLocation)\vkQuake"
 
 $packageArgs = @{
   packageName    = $packageName
-  url            = "https://github.com/Novum/vkQuake/releases/download/1.05.2/vkquake-1.05.2_win64.zip"
-  checksum       = "b7e5ed30a6369a3c2c31ce04dd74ebab064a0038db709cfcc661f70e8a1cb7a9"
-  url64          = "https://github.com/Novum/vkQuake/releases/download/1.05.2/vkquake-1.05.2_win32.zip"
-  checksum64     = "06920832fcd726e74274bd2c4d48e507879fc1ac7ae23cd4eacbbfcd3f9d9a21"
+  url64          = "https://github.com//Novum/vkQuake/releases/download/1.05.3/vkquake-1.05.3_win64.zip"
+  checksum64     = "52c9cfc2821d5685afca7c7494e4db2f60ae7853072d1917ae4bd2f091e39f8a"
+  url            = "https://github.com//Novum/vkQuake/releases/download/1.05.3/vkquake-1.05.3_win32.zip"
+  checksum       = "cb2c4227650fd080cd3bc861b6b97840409c8d84829f595ac25a50573108ca85"
   checksumType   = 'sha256'
   unzipLocation  = $toolsPath
 }
@@ -18,3 +18,5 @@ Robocopy "$vkQuake_latest" "$vkQuake_folder" /R:0 /W:0 /E /XO
 Remove-Item "$vkQuake_latest" -Recurse -Force -ErrorAction SilentlyContinue
 
 Install-ChocolateyShortcut -shortcutFilePath "$env:ALLUSERSPROFILE\Microsoft\Windows\Start Menu\Programs\vkQuake.lnk" "$vkQuake_folder\vkQuake.exe" -WorkingDirectory "$vkQuake_folder"
+
+    

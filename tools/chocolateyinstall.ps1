93a8fa8ea3eada29d87662de067121d27f8caf65
Vladimir Budylnikov <me@nett00n.org>
2022-07-18 11:25:08 +0300

    1.20.3

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,10 +4,10 @@ $vkQuake_folder = "$(Get-ToolsLocation)\vkQuake"
 
 $packageArgs = @{
   packageName    = $packageName
-  url64          = "https://github.com//Novum/vkQuake/releases/download/1.13.1/vkquake-1.13.1_win64.zip"
-  checksum64     = "feb1f124b391e9f6118bdad0c355a6c50df3ebb2a07c7e46e1ba9394dc5a0037"
-  url            = "https://github.com//Novum/vkQuake/releases/download/1.13.1/vkquake-1.13.1_win32.zip"
-  checksum       = "c5abc86211a4de6e09d5e93850545f92afd7108b67a8dfe654e3104f7c1540e3"
+  url64          = "https://github.com//Novum/vkQuake/releases/download/1.20.3/vkquake-1.20.3_win64.zip"
+  checksum64     = "719f57b0ab04d7aa5e39fda3b2b50ad0f32c602d3077e3de9df69ca05b121f06"
+  url            = "https://github.com//Novum/vkQuake/releases/download/1.20.3/vkquake-1.20.3_win32.zip"
+  checksum       = "66ac4236fa16100f39f93071d6533d92eec0d154c067a3052c2e970d9a476e96"
   checksumType   = 'sha256'
   unzipLocation  = $toolsPath
 }
